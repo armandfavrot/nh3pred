477523a3f5b1cdd4e8a067393b3f7e65b72f006e
Armand <armand.favrot@inrae.fr>
2025-10-13 09:10:40 +0200

    Removing unnecessary view application method to x_cont_tensor and x_cat_tensor.

--- src/nh3pred/utils.py
+++ src/nh3pred/utils.py
@@ -7,13 +7,11 @@ def generate_tensors_predictors(df, pmid, device):
 
     x_cont = data_filtered[['ct', 'dt', 'air_temp', 'wind_2m', 'rain_rate', 'tan_app', 'app_rate', 'man_dm', 'man_ph', 't_incorp']]
 
-    x_cont_tensor = torch.tensor(x_cont.values, dtype=torch.float32).view(len(x_cont), len(x_cont.columns))
-    x_cont_tensor = x_cont_tensor.to(device)
-    
+    x_cont_tensor = torch.tensor(x_cont.values, dtype=torch.float32).to(device)
+
     x_cat = data_filtered[['app_mthd', 'incorp', 'man_source']]
     
-    x_cat_tensor = torch.tensor(x_cat.values, dtype=torch.long).view(len(x_cat), len(x_cat.columns))
-    x_cat_tensor = x_cat_tensor.to(device)
+    x_cat_tensor = torch.tensor(x_cat.values, dtype=torch.long).to(device)
     x_cat_tensor = torch.unbind (x_cat_tensor, dim = 1)
 
     output = [x_cont_tensor, x_cat_tensor]
